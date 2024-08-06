f7aca7fa79bd2a7f7e512725a55e53e2d47c06c9
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-08-06 15:05:20 +0300

    gamemanager updated

---
+++ Assets/The Great Fleece/Scrpits/GrabKeyCardActivation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabKeyCardActivation : MonoBehaviour
+{
+    public GameObject sleepingGuardCutScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            sleepingGuardCutScene.SetActive(true);
+            GameManager.Instance.HasCard = true;
+        }
+    }
+}
